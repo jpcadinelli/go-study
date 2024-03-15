dc654a3020ec0de40062d84f58fa187408ce0d58
jpcadinelli <jpcadinelli@gmail.com>
2024-03-14 21:18:16 -0300

    exercício 10

---
+++ exercicios-padrao/010.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main010() {
+	var saldo float32
+	fmt.Print("Insira quanto dinheiro você tem na conta: ")
+	fmt.Scanln(&saldo)
+	dolares := saldo / 3.27
+	fmt.Printf("Você pode comprar %f dolares", dolares)
+}
--- exercicios-padrao/main.go
+++ exercicios-padrao/main.go
@@ -1,5 +1,5 @@
 package main
 
 func main() {
-	main009()
+	main010()
 }
